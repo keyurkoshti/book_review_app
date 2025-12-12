6311c7988d0ec3441e18a7d2cba2b0d09b19cbcb
keyurkoshti <keyurkoshti10@gmail.com>
2025-12-12 20:37:06 +0530

    add admin file

--- book_review/admin.py
+++ book_review/admin.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from .models import Book_Review_forms
 # Register your models here.
 
-admin.site.register(Book_Review_forms)
+
 class BookReviewAdmin(admin.ModelAdmin):
     list_display = ('book_name', 'book_url', 'book_review')  # columns in list view
     search_fields = ('book_name',)  # search bar by book_name
     list_filter = ('book_name',)    # filter by book_name
 
+admin.site.register(Book_Review_forms, BookReviewAdmin)
