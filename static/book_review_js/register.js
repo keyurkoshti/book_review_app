d5c03f552fc95b5107a519f8477977e9a93ff879
keyurkoshti <keyurkoshti10@gmail.com>
2025-12-17 12:48:42 +0530

    add static and template files

---
+++ static/book_review_js/register.js
@@ -0,0 +1,27 @@
+document.getElementById('registerform').addEventListener("submit", async function(event) {
+    event.preventDefault(); //stop refresh page
+
+    const response = await fetch('api/register',{
+        method : "POST",
+        headers : {
+            "content-type" : "application/json",
+            "X-CSRFToken" : "{{ csrf-token }}"
+        },
+        body: JSON.stringify({
+            username : document.getElementById('username').value,
+            email: document.getElementById('email').value,
+            password1: document.getElementById('password1').value,
+            password2: document.getElementById('password2').value,
+        })
+    });
+
+    const data = await response.json()
+
+    if (response.ok){
+        alert("registration successful");
+        window.location.href = "{% url 'login' %}";
+    }
+    else {
+        document.getElementById('error').innerText = "somthing went wrong";
+    }
+});
